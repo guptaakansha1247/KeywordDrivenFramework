620214e87180d168bdb7aad065f4b4efc8dec5cc
Akansha.Gupta <guptaakansha1247@gmail.com>
2016-09-02 17:12:59 +0530

    ok

--- Automation/src/Utility/ExcelUtlity.java
+++ Automation/src/Utility/ExcelUtlity.java
@@ -1,5 +1,37 @@
 package Utility;
-
+import java.io.*;
+import java.io.FileInputStream;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.*;
 public class ExcelUtlity {
-
+	private static XSSFSheet ExcelWSheet;
+    private static XSSFWorkbook ExcelWBook;
+    private static XSSFCell Cell;
+	
+public static void setExcelFile(String Path, String SheetName){
+	try
+	{        
+		/*File file = new File(SheetName);
+	    FileInputStream fIP = new FileInputStream(file);
+	      //Get the workbook instance for XLSX file 
+	    XSSFWorkbook workbook = new XSSFWorkbook(Path);
+	    XSSFSheet sheet=workbook.getSheetAt(0);*/
+	    FileInputStream ExcelFile = new FileInputStream(Path);
+	   File
+        ExcelWBook = new XSSFWorkbook(Path);
+        ExcelWSheet = ExcelWBook.getSheetAt(0);
+	    
+	}
+	catch(Exception ex)
+	{
+	
+	}
 }
+public static String getCellData(int RowNum, int ColNum){
+	Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+    String CellData = Cell.getStringCellValue();
+    return CellData;
+}
+}
+
